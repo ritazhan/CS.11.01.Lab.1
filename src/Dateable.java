171f51dd92deec55592622f95c4d5fa81ada6db6
zhanrita <ritaz2027@student.cis.edu.hk>
2024-08-28 11:19:04 +0800

    finish lab 1

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner diddy = new Scanner(System.in);
+        System.out.println("Enter your age please:");
+        int age = diddy.nextInt();
+        System.out.println(age + " year olds should date someone at least " + ( 7 + ( age / 2 ) ));
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner norris = new Scanner(System.in);
+        System.out.println("Enter your age please:");
+        int age = norris.nextInt();
+        System.out.println(age + " year olds should read at least " + ( 100 - age ) + " pages before giving up on a book");
     }
 }
